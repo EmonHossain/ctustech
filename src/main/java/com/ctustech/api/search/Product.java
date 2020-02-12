3ff7ca0f71694195c1dcc9c89f1d49f2d8dc17b4
Emon <emondipu@gmail.com>
2020-02-12 13:45:09 +0100

    init

---
+++ src/main/java/com/ctustech/api/search/Product.java
@@ -0,0 +1,13 @@
+package com.ctustech.api.search;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+}
